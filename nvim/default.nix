84ad09e613afca1ea7f435c69296bb5554debbea
Mike <mike.meisinger@gmail.com>
2023-04-14 17:39:26 -0400

    Create default.nix

---
+++ nvim/default.nix
@@ -0,0 +1,117 @@
+{ pkgs, config, ... }:
+let
+  leaderKey = "\\<Space>";
+in
+{
+  programs.neovim = {
+    enable = true;
+    vimAlias = true;
+    extraPackages = [
+      pkgs.omnisharp-roslyn
+      pkgs.nodePackages.typescript
+      pkgs.nodePackages.typescript-language-server
+      pkgs.nodePackages.bash-language-server
+      pkgs.nodePackages.vim-language-server
+      pkgs.nodePackages.yaml-language-server
+      pkgs.rnix-lsp
+      pkgs.lua-language-server
+      pkgs.stylua
+      pkgs.shfmt
+      pkgs.nodePackages.eslint
+      pkgs.nodePackages.prettier
+      pkgs.nodePackages.cspell
+    ];
+    extraConfig = ''
+      let mapleader = "${leaderKey}"
+
+      lua << EOF
+        local tsserver_path = "${pkgs.nodePackages.typescript-language-server}/bin/typescript-language-server"
+        local typescript_path = "${pkgs.nodePackages.typescript}/lib/node_modules/typescript/lib"
+        ${builtins.readFile ./init.lua}
+        ${builtins.readFile ./lua_scripts/options.lua}
+        ${builtins.readFile ./lua_scripts/mappings.lua}
+        ${builtins.readFile ./lua_scripts/configuration.lua}
+      EOF
+    '';
+    plugins = with pkgs.vimPlugins; [
+      vim-nix
+      vim-colorschemes
+      gruvbox
+      tokyonight-nvim
+      nvim-web-devicons
+
+      vim-polyglot
+      vim-floaterm
+
+      which-key-nvim
+      nvim-autopairs
+      plenary-nvim
+      gitsigns-nvim
+      popup-nvim
+      lualine-nvim
+      nvim-gps
+      comment-nvim
+
+      telescope-nvim
+      telescope-file-browser-nvim
+      telescope-ui-select-nvim
+      telescope-fzf-native-nvim
+
+      nvim-cmp
+      cmp-nvim-lsp
+      cmp-buffer
+      cmp-path
+      cmp-tmux
+      cmp_luasnip
+
+      nvim-lspconfig
+      null-ls-nvim
+
+      (nvim-treesitter.withPlugins (
+        plugins:
+          with plugins; [
+            tree-sitter-lua
+            tree-sitter-vim
+            tree-sitter-html
+            tree-sitter-yaml
+            tree-sitter-json
+            tree-sitter-markdown
+            tree-sitter-comment
+            tree-sitter-bash
+            tree-sitter-javascript
+            tree-sitter-nix
+            tree-sitter-typescript
+            tree-sitter-c_sharp
+            tree-sitter-query # for the tree-sitter itself
+            tree-sitter-dockerfile
+          ]
+      ))
+
+      luasnip
+      lspkind-nvim
+      friendly-snippets
+
+      nvim-neoclip-lua
+      indent-blankline-nvim
+      nvim-tree-lua
+      vim-tmux-clipboard
+      symbols-outline-nvim
+
+      noice-nvim
+      nui-nvim
+      fidget-nvim
+      nvim-lightbulb
+
+      neoscroll-nvim
+      neogit
+      undotree
+      diffview-nvim
+      goto-preview
+      nvim-dap
+      trouble-nvim
+      lsp_lines-nvim
+
+      omnisharp-extended-lsp-nvim
+    ];
+  };
+}
